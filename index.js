286169df6e3ec8a0443bf1f0b74b3c784df7fa06
Sean King <sean@seanking.org>
2015-07-23 12:16:17 +1000

    pass actual hex to coverter (derp) and refactor

--- index.js
+++ index.js
@@ -5,81 +5,91 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-hexrgba', function () {
   return function (css) {
 
-    // build hex -> converter (returns array)
+    /**
+     * Hex to RGB converter
+     * @param  {string} hex hexidecimal string
+     * @return {array} RGB values
+     */
     var hexRgb = function(hex){
 
-      // strip the #
+      // Strip the #
       var stripped = hex.split('');
       stripped.shift();
       hex = stripped.join('');
 
-      // if given a shorthand decleration, expand it
+      // If given shorthand, expand it
       var shorthandCheck = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
       hex = hex.replace(shorthandCheck, function(m, r, g, b) {
           return r + r + g + g + b + b;
       });
 
-      // convert it
+      // Convert it
       var convert = parseInt(hex, 16);
       var red = convert >> 16 & 255;
       var green = convert >> 8 & 255;
       var blue = convert & 255;
 
-      // throw the results into an array
+      // Throw the results into an array
       hex = [red, green, blue];
 
       return hex;
 
     };
 
-    // build our plugin handler
+    /**
+     * CSS rule handler
+     * @param  {string} decl CSS delcaration
+     */
     var ruleHandler = function(decl) {
 
       var input = decl.value;
       var output = '';
 
-      // only process rgba declaration values
-      if (input.indexOf('rgba') === -1) {
-        return false;
-      }
-
-      // strip out surrounding css property and throw the values in an array
+      // Strip out surrounding css property and throw the values in an array
       var rgba = input.split('(')[1].split(')')[0];
       rgba = rgba.split(',');
 
-      // only process rgba values with 2 arguments (hex + alpha)
+      // Only process rgba values with 2 arguments (hex + alpha)
       if (rgba.length > 2){
-        return false;
+        return;
       }
 
-      // store the alpha value for safe keeping
+      // Store the alpha value for safe keeping
       var a = rgba[1];
 
-      // and strip any trailing spaces
+      // And strip any trailing spaces
       a = a.replace(/\s/g, '');
 
-      // rip out the alpha and turn array into hex string
+      // Rip out the alpha and turn array into hex string
       var hex = rgba.splice(0, 1);
       hex = hex.toString();
 
-      // feed it to our converter
-      var rgb = hexRgb('#ffffff');
+      // Feed it to our converter
+      var rgb = hexRgb(hex);
 
-      // add the alpha back in
+      // Add the alpha back in
       rgb.push(a);
 
-      // convert the whole thing to a string again and add back in css wrapper
+      // Convert the whole thing to a string again and add back in css wrapper
       output = rgb.toString();
       output = 'rgba(' + output + ')';
 
-      // create the new declaration value
+      // Create the new declaration value
       decl.value = output;
 
     };
 
-    // loop through each css rule and declaration, and run our plugin through them
-    css.eachRule(function(rule) {
-      rule.each(ruleHandler);
+    // Loop through each css rule and declaration, and run our plugin through them
+    css.eachDecl(function(decl) {
+
+      // Only process rgba declaration values
+      if (decl.value.indexOf('rgba') === -1) {
+        return;
+      }
+
+      // Pass the relevant decls to our rule handler
+      ruleHandler(decl);
+
     });
 
   };
