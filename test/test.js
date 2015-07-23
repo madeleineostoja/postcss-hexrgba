0c5a1445d3e05630342e4707bc42b2570f814cad
Sean King <sean@seanking.org>
2015-07-23 12:15:49 +1000

    refactor tests

--- test/test.js
+++ test/test.js
@@ -3,33 +3,37 @@
 
 var postcss = require('postcss'),
     expect = require('chai').expect,
+    fs = require('fs'),
+    path = require('path'),
     plugin = require('../');
 
-var test = function (input, output, opts, done) {
-  postcss([ plugin(opts) ]).process(input).then(function (result) {
-    expect(result.css).to.eql(output);
-    expect(result.warnings()).to.be.empty;
+var test = function(fixture, opts, done) {
+  var input = fixture + '.css',
+      expected = fixture + '.expected.css';
+
+  input = fs.readFileSync(path.join(__dirname, 'fixtures', input), 'utf8');
+  expected = fs.readFileSync(path.join(__dirname, 'fixtures', expected), 'utf8');
+
+  postcss([ plugin(opts) ])
+    .process(input)
+    .then(function (result) {
+      expect(result.css).to.eql(expected);
+      expect(result.warnings()).to.be.empty;
     done();
   }).catch(function (error) {
     done(error);
   });
+
 };
 
-describe('postcss-hexrgba', function () {
+describe('postcss-hexrgba', function() {
 
-  it('handles standard hex', function (done) {
-    test('a{ color: rgba(#fffff,0.8); }',
-         'a{ color: rgba(255,255,255,0.8); }', { }, done);
+  it('handles standard hex', function(done) {
+    test('standard', {}, done);
   });
 
-  it('handles shorthand hex', function (done) {
-    test('a{ color: rgba(#fff,1); }',
-         'a{ color: rgba(255,255,255,1); }', { }, done);
-  });
-
-  it('handles spaces in rgba', function (done) {
-    test('a{ color: rgba( #fff , .5 ); }',
-         'a{ color: rgba(255,255,255,.5); }', { }, done);
+  it('handles shorthand hex', function(done) {
+    test('shorthand', {}, done);
   });
 
 });
